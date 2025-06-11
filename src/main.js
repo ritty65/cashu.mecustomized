c06149b66c97cd7a82226f013d34b4006c37ffdd
ritty65 <contact@simulacraproductions.net>
2025-06-11 10:38:13 +0200

    chore: initialize app with router store quasar

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import registerIcons from "./icons";
 
+import { Quasar } from "quasar";
+import router from "./router";
+import pinia from "./stores";
+import { i18n } from "./boot/i18n";
+
 const app = createApp(App);
 registerIcons(app);
+
+app.use(Quasar);
+app.use(router);
+app.use(pinia);
+app.use(i18n);
+app.mount("#q-app");
