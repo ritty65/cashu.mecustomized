93a156a56c18a89e3100c1a3e32b3470df6d66ee
ritty65 <contact@simulacraproductions.net>
2025-09-05 06:42:55 +0200

    feat: load safe area plugin dynamically

--- src/boot/base.js
+++ src/boot/base.js
@@ -1,6 +1,6 @@
 import { useUiStore } from "stores/ui";
 import { Clipboard } from "@capacitor/clipboard";
-import { SafeArea } from "capacitor-plugin-safe-area";
+
 
 window.LOCALE = "en";
 // window.EventHub = new Vue();
@@ -220,25 +220,33 @@ window.windowMixin = {
 
     // only for iOS
     if (window.Capacitor && Capacitor.getPlatform() === "ios") {
-      SafeArea.getStatusBarHeight().then(({ statusBarHeight }) => {
-        document.documentElement.style.setProperty(
-          `--safe-area-inset-top`,
-          `${statusBarHeight}px`
-        );
-      });
+      import(
+        /* @vite-ignore */ "capacitor-plugin-safe-area"
+      )
+        .then(({ SafeArea }) => {
+          SafeArea.getStatusBarHeight().then(({ statusBarHeight }) => {
+            document.documentElement.style.setProperty(
+              `--safe-area-inset-top`,
+              `${statusBarHeight}px`
+            );
+          });
 
-      SafeArea.removeAllListeners();
+          SafeArea.removeAllListeners();
 
-      // when safe-area changed
-      SafeArea.addListener("safeAreaChanged", (data) => {
-        const { insets } = data;
-        for (const [key, value] of Object.entries(insets)) {
-          document.documentElement.style.setProperty(
-            `--safe-area-inset-${key}`,
-            `${value}px`
-          );
-        }
-      });
+          // when safe-area changed
+          SafeArea.addListener("safeAreaChanged", (data) => {
+            const { insets } = data;
+            for (const [key, value] of Object.entries(insets)) {
+              document.documentElement.style.setProperty(
+                `--safe-area-inset-${key}`,
+                `${value}px`
+              );
+            }
+          });
+        })
+        .catch(() => {
+          // plugin not available
+        });
     }
   },
 };
